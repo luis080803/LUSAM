5961ef23065d9dac4c72b971f00d9c26ea46fd33
SamuelVelandia <obed.gvelandia@gmail.com>
2025-06-09 19:46:23 -0500

    encriptación de preguntas

--- app/models/preguntasrecuperacion.py
+++ app/models/preguntasrecuperacion.py
@@ -1,7 +1,14 @@
 from pydantic import BaseModel, Field, validator
 from typing import List
+from app.utils.password_utils import hash_password
 
 class PreguntasSeguridadBase(BaseModel):
     Usuario: str = Field(..., min_length=4, max_length=20, description="Usuario")
-    preguntas: List[str] = Field(..., min_items=2, max_items=2, description="Lista de 3 preguntas de seguridad")
-    respuestas: List[str] = Field(..., min_items=2, max_items=2, description="Lista de 3 respuestas correspondientes")
+    preguntas: List[str] = Field(..., min_items=2, max_items=2, description="Lista de 2 preguntas de seguridad")
+    respuestas: List[str] = Field(..., min_items=2, max_items=2, description="Lista de 2 respuestas correspondientes")
+
+    def model_dump(self, *args, **kwargs):
+        data = super().model_dump(*args, **kwargs)
+        # Hashear las respuestas antes de guardar
+        data['respuestas'] = [hash_password(resp) for resp in data['respuestas']]
+        return data
